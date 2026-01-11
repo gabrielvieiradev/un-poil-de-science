0159c05d5bf407098f9aeb4b24813849e77dfe80
Gabriel Vieira <gabriel93250@gmail.com>
2026-01-11 21:31:11 +0100

    Implement stack

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { StackProvider, StackTheme } from "@stackframe/stack";
+import { stackClientApp } from "../stack/client";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NavBar } from "@/components/nav-bar";
 
@@ -28,10 +30,10 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><StackProvider app={stackClientApp}><StackTheme>
         <NavBar />
         {children}
-      </body>
+      </StackTheme></StackProvider></body>
     </html>
   );
 }
